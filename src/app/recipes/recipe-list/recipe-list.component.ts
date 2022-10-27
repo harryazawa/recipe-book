b1388a7527a888f89411bd6a7837acfdbb9ae8be
harryazawa <victorlbamorim@gmail.com>
2022-10-27 15:29:59 -0300

    feat: components for header, recipe list and recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a Test Recipe',
+      'https://www.foodandwine.com/thmb/x3xYEfsVqTTDHkXsk2eQGdsatPk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-swiss-cheese-fondue-FT-RECIPE0422-35e22a24bcb24899840568271197377b.jpg'),
+    new Recipe('A Test Recipe', 'This is a Test Recipe',
+      'https://www.foodandwine.com/thmb/x3xYEfsVqTTDHkXsk2eQGdsatPk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-swiss-cheese-fondue-FT-RECIPE0422-35e22a24bcb24899840568271197377b.jpg')
+  ];
 
   constructor() { }
 
