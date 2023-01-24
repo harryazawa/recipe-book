f567c94b752c4cd7577ec88b96ffbe5f3d3f60db
harryazawa <victorlbamorim@gmail.com>
2023-01-24 16:26:44 -0300

    refactor: passing recipe data with property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -7,16 +7,21 @@ import {Recipe} from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a Test Recipe',
       'https://www.foodandwine.com/thmb/x3xYEfsVqTTDHkXsk2eQGdsatPk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-swiss-cheese-fondue-FT-RECIPE0422-35e22a24bcb24899840568271197377b.jpg'),
-    new Recipe('A Test Recipe', 'This is a Test Recipe',
+    new Recipe('Another Test Recipe', 'This is a Test Recipe',
       'https://www.foodandwine.com/thmb/x3xYEfsVqTTDHkXsk2eQGdsatPk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-swiss-cheese-fondue-FT-RECIPE0422-35e22a24bcb24899840568271197377b.jpg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
